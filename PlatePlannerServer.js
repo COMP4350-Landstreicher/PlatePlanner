f432e07e5d6f6e93957508c8b9dc952dfa345063
David Dyck <git@ddyck.ca>
2023-02-05 20:57:34 -0600

    Added dockerfiles for DB and Webserver, and basic node setup

---
+++ PlatePlannerServer.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+
+const app = express();
+app.get('/', (req, res) => {res.send('<h1>Welcome to PlatePlanner!</h1>');});
+
+app.listen(PORT, HOST, () => {console.log(`Running on http://${HOST}:${PORT}`);});
