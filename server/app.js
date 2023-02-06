281c6806abb9c41d9309f36789355e1931b08906
TuanDinhLe <dinhtuan2201@yahoo.com.vn>
2023-02-06 16:51:20 -0600

    Add client and server for the crud web app.

---
+++ server/app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const DBService = require('./DBService');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+
+// add recipe
+app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = DBService.getDBServiceInstance();
+    
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({ data: data}))
+    .catch(err => console.log(err));
+});
+
+// get all recipes
+app.get('/getAll', (request, response) => {
+    console.log("Test passed")
+    const db = DBService.getDBServiceInstance();
+
+    const result = db.getAllData();
+    
+    // cannot return nything here, still need to wait
+    // on the promises from DBService.getAllData()
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+// update recipe
+app.patch('/update', (request, response) => {
+    const { id, name } = request.body;
+    const db = DBService.getDBServiceInstance();
+
+    const result = db.updateNameById(id, name);
+    
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+});
+
+// delete recipe
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = DBService.getDBServiceInstance();
+
+    const result = db.deleteRowById(id);
+    
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+});
+
+// search recipe
+app.get('/search/:name', (request, response) => {
+    const { name } = request.params;
+    const db = DBService.getDBServiceInstance();
+
+    const result = db.searchByName(name);
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+app.listen(process.env.PORT, () => console.log(`app is running on port ${process.env.PORT}`));
---
+++ server/dbService.js
@@ -0,0 +1,128 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+// get credentials from .env
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err.message);
+    }
+    // console.log('db ' + connection.state);
+});
+
+
+class DBService {
+    // If not already exist, create a new instance of DBService
+    static getDBServiceInstance() {
+        return instance ? instance : new DBService();
+    }
+
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM recipe_dev;";
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            // console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    async insertNewName(name) {
+        try {
+            const dateAdded = new Date();
+            const insertId = await new Promise((resolve, reject) => {
+                const query = "INSERT INTO recipe_dev (name, last_updated) VALUES (?,?);";
+                
+                // prevent SQL injection bt passing in values separatedly via [name, dateAdded]
+                connection.query(query, [name, dateAdded] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.insertId);
+                })
+            });
+            return {
+                id : insertId,
+                name : name,
+                dateAdded : dateAdded
+            };
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async deleteRowById(id) {
+        try {
+            id = parseInt(id, 10); 
+            const response = await new Promise((resolve, reject) => {
+                const query = "DELETE FROM recipe_dev WHERE id = ?";
+    
+                connection.query(query, [id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+    
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async updateNameById(id, name) {
+        try {
+            // Parse using base 10
+            id = parseInt(id, 10); 
+            // if there is any error when querying, a 'reject will be returned'
+            // and the catch block will be executed
+            const response = await new Promise((resolve, reject) => {
+                const query = "UPDATE recipe_dev SET name = ? WHERE id = ?";
+    
+                connection.query(query, [name, id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+    
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async searchByName(name) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM recipe_dev WHERE name = ?;";
+
+                connection.query(query, [name], (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = DBService;
