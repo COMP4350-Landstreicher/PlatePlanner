46491571d96a3f00b6cc8fa4ca097e785e33b957
MatthiasPenner <matthias.penner10@gmail.com>
2023-02-13 15:12:49 -0600

    PLATE-26 Fixed formatting issue

--- server/PlatePlannerServer.js
+++ server/PlatePlannerServer.js
@@ -24,19 +24,20 @@ app.get('/test', authenticate, (req, res) => {res.send('test');});
 
 app.use('/auth', require('./routes/authRoutes'));
 
-app.use(errorHandler);
 app.listen(PORT, HOST, () => {console.log(`Running on http://${HOST}:${PORT}`);});
 app.get("/recipes", (req, res) => {
-	    const recipes = [
-		            {id: 1, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
-		            {id: 2, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
-		            {id: 3, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
-		            {id: 4, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
-		            {id: 5, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
-		            {id: 6, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
-		            {id: 7, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
-		            {id: 8, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
-		            {id: 9, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"}
-		        ]
-	    res.send(recipes);
+	const recipes = [
+		{id: 1, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
+		{id: 2, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
+		{id: 3, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
+		{id: 4, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
+		{id: 5, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
+		{id: 6, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
+		{id: 7, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
+		{id: 8, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"},
+		{id: 9, name: "This is recipe nameeeee", image: "https://source.unsplash.com/random"}
+		]
+	res.send(recipes);
 });
+
+app.use(errorHandler);
