1b7e253e04fb8afe26aa70a073f066d8e395d34e
TuanDinhLe <dinhtuan2201@yahoo.com.vn>
2023-03-06 17:48:04 -0600

    Refactor test cases after adding createNewRecipe().

--- server/tests/mocks/recipeMock.js
+++ server/tests/mocks/recipeMock.js
@@ -1,59 +1,23 @@
 class RecipeMock{
 	constructor(){
-		this.recipe_name = "roasted_eggplant";
-        this.description = "tasty_dish";
-        this.instructions = "mix all the good food";
-        this.selected = true;
-        this.user_id = 10;
-        this.id = 1;
-        this.lastUpdated = "16/02/2023";
-        this.imageUrl = "abc";
+        this.recipe = null
 	}
 
-	sync(){
-
-	}
+	sync(){}
 
     findAll(arg){
-        if (this.user_id == arg.where.user_id) {
-            return [{
-                recipe_name:this.recipe_name, 
-                description:this.description, 
-                instructions:this.instructions, 
-                selected: this.selected, 
-                user_id:this.user_id, 
-                id:this.id, 
-                lastUpdated:this.lastUpdated,
-                imageUrl: this.imageUrl
-            }]
+        if (this.recipe.user_id == arg.where.user_id) {
+            return [this.recipe]
         }
         return [];
     }
 
 	findOne(arg){
-		if(this.recipe_name == arg.where.recipe_name){
-			return {
-                recipe_name:this.recipe_name, 
-                description:this.description, 
-                instructions:this.instructions, 
-                selected: this.selected, 
-                user_id:this.user_id, 
-                id:this.id, 
-                lastUpdated:this.lastUpdated,
-                imageUrl: this.imageUrl
-            }
+		if(this.recipe.recipe_name == arg.where.recipe_name){
+			return this.recipe
 		}
-        else if(this.id == arg.where.id){
-            return {
-                recipe_name:this.recipe_name, 
-                description:this.description, 
-                instructions:this.instructions, 
-                selected: this.selected, 
-                user_id:this.user_id, 
-                id:this.id, 
-                lastUpdated:this.lastUpdated,
-                imageUrl: this.imageUrl
-            }
+        else if(this.recipe.id == arg.where.id){
+            return this.recipe
         }
 		return null
 	}
@@ -61,6 +25,19 @@ class RecipeMock{
     bulkCreate(){
         return [];
     }
+
+    create(recipe){
+        if (this.recipe != null) {
+            if (this.recipe.recipe_name == recipe.recipe_name){
+                return null
+            }
+        }
+
+        recipe["id"] = 1
+        recipe["lastUpdated"] = "16/02/2023"
+		this.recipe = recipe
+		return recipe
+	}
 }
 
 module.exports = { RecipeMock }
--- server/tests/recipeTest.js
+++ server/tests/recipeTest.js
@@ -1,4 +1,4 @@
-const {getAllRecipe, getByName, getByID} = require("../utils/recipeUtils")
+const {getAllRecipe, getByName, getByID, createNewRecipe} = require("../utils/recipeUtils")
 const {RecipeMock} = require("./mocks/recipeMock")
 const expect  = require('chai').expect;
 
@@ -6,45 +6,48 @@ describe("Backend recipe tests", () => {
 it("Get all recipes belong to a valid user", async () => {
 	const Recipe = new RecipeMock();
 	const mockUserID = 10;
-	const recipes = await getAllRecipe(10, Recipe);
+	await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
+	const recipes = await getAllRecipe(mockUserID, Recipe);
 
 	expect(recipes).to.be.an("array");
 	expect(recipes[0]).to.be.an("object");
 	expect(recipes[0].recipe_name).to.equal("roasted_eggplant");
 	expect(recipes[0].description).to.equal("tasty_dish");
 	expect(recipes[0].instructions).to.equal("mix all the good food");
-	expect(recipes[0].selected).to.equal(true);
+	expect(recipes[0].selected).to.equal(false);
 	expect(recipes[0].user_id).to.equal(10);
-	expect(recipes[0].id).to.equal(1);
-	expect(recipes[0].lastUpdated).to.equal("16/02/2023");
 	expect(recipes[0].imageUrl).to.equal("abc");
+	expect(recipes[0].lastUpdated).to.equal("16/02/2023");
+	expect(recipes[0].id).to.equal(1);
 });
 
 it("Get all recipes belong to an invalid user", async () => {
 	const Recipe = new RecipeMock();
-	const mockUserID = 2;
-	const recipes = await getAllRecipe(mockUserID, Recipe);
+	await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
+	const recipes = await getAllRecipe(2, Recipe);
 
 	expect(recipes.length).to.equal(0);
 });
 
 it("Get a recipe by Name", async () => {
 	const Recipe = new RecipeMock();
+	await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
 	const recipe = await getByName("roasted_eggplant", 10, Recipe);
 
 	expect(recipe).to.be.an("object");
 	expect(recipe.recipe_name).to.equal("roasted_eggplant");
 	expect(recipe.description).to.equal("tasty_dish");
 	expect(recipe.instructions).to.equal("mix all the good food");
-	expect(recipe.selected).to.equal(true);
+	expect(recipe.selected).to.equal(false);
 	expect(recipe.user_id).to.equal(10);
-	expect(recipe.id).to.equal(1);
-	expect(recipe.lastUpdated).to.equal("16/02/2023");
 	expect(recipe.imageUrl).to.equal("abc");
+	expect(recipe.lastUpdated).to.equal("16/02/2023");
+	expect(recipe.id).to.equal(1);
 });
 
 it("Get a recipe by Name that does not exist", async () => {
 	const Recipe = new RecipeMock();
+	await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
 	const recipe = await getByName("roasted_tomato", 10, Recipe);
 
 	expect(recipe).to.equal(null);
@@ -52,6 +55,7 @@ it("Get a recipe by Name that does not exist", async () => {
 
 it("Get a recipe with a valid recipeID", async () => {
 	const Recipe = new RecipeMock();
+	await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
 	const recipe = await getByID(1, Recipe);
 
 	expect(recipe).to.be.an("object");
@@ -59,15 +63,41 @@ it("Get a recipe with a valid recipeID", async () => {
 	expect(recipe.recipe_name).to.equal("roasted_eggplant");
 	expect(recipe.description).to.equal("tasty_dish");
 	expect(recipe.instructions).to.equal("mix all the good food");
-	expect(recipe.selected).to.equal(true);
-	expect(recipe.lastUpdated).to.equal("16/02/2023");
+	expect(recipe.selected).to.equal(false);
 	expect(recipe.imageUrl).to.equal("abc");
+	expect(recipe.lastUpdated).to.equal("16/02/2023");
+	expect(recipe.id).to.equal(1);
 });
 
 it("Get a recipe with an invalid recipeID", async () => {
 	const Recipe = new RecipeMock();
+	await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
 	const recipe = await getByID(2, Recipe);
 
 	expect(recipe).to.equal(null);
 });
+
+it("Create a new recipe", async () => {
+	const Recipe = new RecipeMock();
+	const recipe = await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
+
+	expect(recipe).to.be.an("object");
+	expect(recipe.id).to.equal(1);
+	expect(recipe.recipe_name).to.equal("roasted_eggplant");
+	expect(recipe.description).to.equal("tasty_dish");
+	expect(recipe.instructions).to.equal("mix all the good food");
+	expect(recipe.selected).to.equal(false);
+	expect(recipe.imageUrl).to.equal("abc");
+	expect(recipe.lastUpdated).to.equal("16/02/2023");
+	expect(recipe.id).to.equal(1);
+});
+
+it("Create a duplicated recipe", async () => {
+	const Recipe = new RecipeMock();
+	await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
+	const recipe = await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
+	
+	expect(recipe).to.equal(null);
+});
+
 })
