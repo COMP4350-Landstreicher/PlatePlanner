fe2a2bc471b73a767992e07db247d15ba6d337f7
TuanDinhLe <dinhtuan2201@yahoo.com.vn>
2023-03-16 13:32:20 -0500

    Add unit tests for update and delete recips.

--- server/tests/recipeTest.js
+++ server/tests/recipeTest.js
@@ -96,5 +96,64 @@ it("Get a recipe with an invalid recipeID", async () => {
 	expect(recipe).to.equal(null);
 });
 
+it("Remove a recipe with a valid recipeID", async () => {
+	const Recipe = new RecipeMock();
+	await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
+	const recipe = await getByID(1, Recipe);
+
+	expect(recipe).to.be.an("object");
+	expect(recipe.id).to.equal(1);
+	expect(recipe.recipeName).to.equal("roasted_eggplant");
+	expect(recipe.description).to.equal("tasty_dish");
+	expect(recipe.instructions).to.equal("mix all the good food");
+	expect(recipe.imageURL).to.equal("abc");
+	expect(recipe.lastUpdated).to.equal("16/02/2023");
+
+	const result = await removeRecipe(1, Recipe)
+	expect(result).to.equal(true);
+
+	const removed_recipe = await getByID(1, Recipe);
+	expect(removed_recipe).to.equal(null);
+});
+
+it("Remove a recipe that does not exist", async () => {
+	const Recipe = new RecipeMock();
+	const result = await removeRecipe(1, Recipe)
+	expect(result).to.equal(false);
+});
+
+it("Update a recipe", async () => {
+	const Recipe = new RecipeMock();
+	const recipe = await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
+
+	expect(recipe).to.be.an("object");
+	expect(recipe.id).to.equal(1);
+	expect(recipe.recipeName).to.equal("roasted_eggplant");
+	expect(recipe.description).to.equal("tasty_dish");
+	expect(recipe.instructions).to.equal("mix all the good food");
+	expect(recipe.imageURL).to.equal("abc");
+	expect(recipe.lastUpdated).to.equal("16/02/2023");
+	expect(recipe.userID).to.equal(10);
+
+	const updated_recipe = await updateRecipeByID(1, "vietnamese_pho", "more_tasty_dish", "mix all the good food", "def", Recipe)
+
+	expect(updated_recipe).to.be.an("object");
+	expect(updated_recipe.id).to.equal(1);
+	expect(updated_recipe.recipeName).to.equal("vietnamese_pho");
+	expect(updated_recipe.description).to.equal("more_tasty_dish");
+	expect(updated_recipe.instructions).to.equal("mix all the good food");
+	expect(updated_recipe.imageURL).to.equal("def");
+	expect(updated_recipe.lastUpdated).to.equal("17/02/2023");
+	expect(updated_recipe.userID).to.equal(10);
+});
+
+it("Update a non-existing recipe", async () => {
+	const Recipe = new RecipeMock();
+	await createNewRecipe("roasted_eggplant", "tasty_dish", "mix all the good food", "abc", 10, Recipe)
+	const updated_recipe = await updateRecipeByID(2, "vietnamese_pho", "more_tasty_dish", "mix all the good food", "def", Recipe)
+	
+	expect(updated_recipe).to.equal(null);
+});
+
 
 })
