7bc54cbd32e9a99063d2988a0ee9938d3a23961b
TuanDinhLe <dinhtuan2201@yahoo.com.vn>
2023-03-29 13:02:18 -0500

    Reformating front-end tests.

--- client/src/tests/ingredients.test.js
+++ client/src/tests/ingredients.test.js
@@ -1,30 +1,32 @@
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import ShoppingList, { genMailList } from '../components/shoppingList';
+import {MemoryRouter} from 'react-router-dom';
+import ShoppingList, {genMailList} from '../components/shoppingList';
 import IngredientList from '../components/ingredientList';
 
 
-it("Should render ShoppingList component", () => {
-    render(<ShoppingList />, { wrapper: MemoryRouter });
-})
+it('Should render ShoppingList component', () => {
+  render(<ShoppingList />, {wrapper: MemoryRouter});
+});
 
-it("should render IngredientList component", () => {
-    render(<IngredientList value={[{ ingredientName: "Name", ingredientUnit: "Unit", totalAmount: 22 }]} />, { wrapper: MemoryRouter });
-})
+it('should render IngredientList component', () => {
+  render(<IngredientList value={[{ingredientName: 'Name', ingredientUnit: 'Unit', totalAmount: 22}]} />, {wrapper: MemoryRouter});
+});
 
 
-it("Generating mail without any ingredients should produce an empty email", () => {
-    var ingredients = [];
-    expect(genMailList(ingredients)).toEqual('mailto:?subject=Shopping%20List&body=Shopping%20List%3A%20%0D%0A');
-})
+it('Generating mail without any ingredients should produce an empty email', () => {
+  const ingredients = [];
+  expect(genMailList(ingredients)).toEqual('mailto:?subject=Shopping%20List&body=Shopping%20List%3A%20%0D%0A');
+});
 
-it("Generating mail with valid ingredients should produce a valid email", () => {
-    var ingredients = [{ ingredientName: "Name", ingredientUnit: "Unit", totalAmount: 22 }];
-    expect(genMailList(ingredients)).toEqual('mailto:?subject=Shopping%20List&body=Shopping%20List%3A%20%0D%0AName%20-%2022%20Unit%0D%0A');
-})
+it('Generating mail with valid ingredients should produce a valid email', () => {
+  const ingredients = [{ingredientName: 'Name', ingredientUnit: 'Unit', totalAmount: 22}];
+  expect(genMailList(ingredients)).toEqual('mailto:?subject=Shopping%20List&body=Shopping%20List%3A%20%0D%0AName%20-%2022%20Unit%0D%0A');
+});
 
-it("Generating mail with garbage data should produce an error", () => {
-    var ingredients = "COWS";
-    expect(() => { genMailList(ingredients) }).toThrow();
-})
+it('Generating mail with garbage data should produce an error', () => {
+  const ingredients = 'COWS';
+  expect(() => {
+    genMailList(ingredients);
+  }).toThrow();
+});
--- client/src/tests/recipe.test.js
+++ client/src/tests/recipe.test.js
@@ -1,170 +1,170 @@
-import { render } from '@testing-library/react';
-import { search } from 'components/searchBar';
-import { validateForm } from 'components/addRecipePopup';
+import {render} from '@testing-library/react';
+import {search} from 'components/searchBar';
+import {validateForm} from 'components/addRecipePopup';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import Recipes, { sortByDate, sortByName } from "../components/recipes";
+import {MemoryRouter} from 'react-router-dom';
+import Recipes, {sortByDate, sortByName} from '../components/recipes';
 
-it("should render Homepage component", () => {
-  render(<Recipes />, { wrapper: MemoryRouter });
-})
+it('should render Homepage component', () => {
+  render(<Recipes />, {wrapper: MemoryRouter});
+});
 
-it("should return search result", () => {
+it('should return search result', () => {
   const recipes = [
-    { recipeName: "A" },
-    { recipeName: "B" },
-    { recipeName: "C" }
+    {recipeName: 'A'},
+    {recipeName: 'B'},
+    {recipeName: 'C'},
   ];
-  expect(search(recipes, "A")).toHaveLength(1);
-  expect(search(recipes, "A")).toEqual([{ recipeName: "A" }]);
-})
+  expect(search(recipes, 'A')).toHaveLength(1);
+  expect(search(recipes, 'A')).toEqual([{recipeName: 'A'}]);
+});
 
-it("should return no search result", () => {
+it('should return no search result', () => {
   const recipes = [
-    { recipeName: "A" },
-    { recipeName: "B" },
-    { recipeName: "C" }
+    {recipeName: 'A'},
+    {recipeName: 'B'},
+    {recipeName: 'C'},
   ];
-  expect(search(recipes, "D")).toHaveLength(0);
-  expect(search(recipes, "D")).toEqual([]);
-})
+  expect(search(recipes, 'D')).toHaveLength(0);
+  expect(search(recipes, 'D')).toEqual([]);
+});
 
-it("should return search result even without case sensitive", () => {
+it('should return search result even without case sensitive', () => {
   const recipes = [
-    { recipeName: "A" },
-    { recipeName: "B" },
-    { recipeName: "C" }
+    {recipeName: 'A'},
+    {recipeName: 'B'},
+    {recipeName: 'C'},
   ];
-  expect(search(recipes, "a")).toHaveLength(1);
-  expect(search(recipes, "a")).toEqual([{ recipeName: "A" }]);
-})
+  expect(search(recipes, 'a')).toHaveLength(1);
+  expect(search(recipes, 'a')).toEqual([{recipeName: 'A'}]);
+});
 
-it("should return sorted list by name ascending", () => {
+it('should return sorted list by name ascending', () => {
   const recipes = [
-    { recipeName: "C" },
-    { recipeName: "A" },
-    { recipeName: "B" }
+    {recipeName: 'C'},
+    {recipeName: 'A'},
+    {recipeName: 'B'},
   ];
   const result = [
-    { recipeName: "A" },
-    { recipeName: "B" },
-    { recipeName: "C" }
+    {recipeName: 'A'},
+    {recipeName: 'B'},
+    {recipeName: 'C'},
   ];
   expect(sortByName(true, recipes)).toHaveLength(3);
   expect(sortByName(true, recipes)).toEqual(result);
-})
+});
 
-it("should return sorted list by name descending", () => {
+it('should return sorted list by name descending', () => {
   const recipes = [
-    { recipeName: "C" },
-    { recipeName: "A" },
-    { recipeName: "B" }
+    {recipeName: 'C'},
+    {recipeName: 'A'},
+    {recipeName: 'B'},
   ];
   const result = [
-    { recipeName: "C" },
-    { recipeName: "B" },
-    { recipeName: "A" }
+    {recipeName: 'C'},
+    {recipeName: 'B'},
+    {recipeName: 'A'},
   ];
   expect(sortByName(false, recipes)).toHaveLength(3);
   expect(sortByName(false, recipes)).toEqual(result);
-})
+});
 
-it("should return sorted list by date ascending", () => {
+it('should return sorted list by date ascending', () => {
   const recipes = [
-    { updatedAt: new Date('2023-01-03') },
-    { updatedAt: new Date('2023-01-01') },
-    { updatedAt: new Date('2023-01-02') }
+    {updatedAt: new Date('2023-01-03')},
+    {updatedAt: new Date('2023-01-01')},
+    {updatedAt: new Date('2023-01-02')},
   ];
   const result = [
-    { updatedAt: new Date('2023-01-01') },
-    { updatedAt: new Date('2023-01-02') },
-    { updatedAt: new Date('2023-01-03') }
+    {updatedAt: new Date('2023-01-01')},
+    {updatedAt: new Date('2023-01-02')},
+    {updatedAt: new Date('2023-01-03')},
   ];
   expect(sortByDate(true, recipes)).toHaveLength(3);
   expect(sortByDate(true, recipes)).toEqual(result);
-})
+});
 
-it("should return sorted list by date descending", () => {
+it('should return sorted list by date descending', () => {
   const recipes = [
-    { updatedAt: new Date('2023-01-03') },
-    { updatedAt: new Date('2023-01-01') },
-    { updatedAt: new Date('2023-01-02') }
+    {updatedAt: new Date('2023-01-03')},
+    {updatedAt: new Date('2023-01-01')},
+    {updatedAt: new Date('2023-01-02')},
   ];
   const result = [
-    { updatedAt: new Date('2023-01-03') },
-    { updatedAt: new Date('2023-01-02') },
-    { updatedAt: new Date('2023-01-01') }
+    {updatedAt: new Date('2023-01-03')},
+    {updatedAt: new Date('2023-01-02')},
+    {updatedAt: new Date('2023-01-01')},
   ];
   expect(sortByDate(false, recipes)).toHaveLength(3);
   expect(sortByDate(false, recipes)).toEqual(result);
-})
+});
 
-it("the form should be invalid when required field is blank", () => {
+it('the form should be invalid when required field is blank', () => {
   const recipe = {
-    name: "a",
+    name: 'a',
     ingredients: [{
-      ingredientName: "",
-      ingredientUnit: "count",
-      ingredientAmount: "1"
+      ingredientName: '',
+      ingredientUnit: 'count',
+      ingredientAmount: '1',
     }],
-    directions: ["first step"]
-  }
+    directions: ['first step'],
+  };
   expect(validateForm(recipe.name, recipe.directions, recipe.ingredients)).toEqual(true);
-})
+});
 
-it("the form should be invalid when ingredient name is duplicated", () => {
+it('the form should be invalid when ingredient name is duplicated', () => {
   const recipe = {
-    name: "a",
+    name: 'a',
     ingredients: [{
-      ingredientName: "sugar",
-      ingredientUnit: "g",
-      ingredientAmount: "2"
+      ingredientName: 'sugar',
+      ingredientUnit: 'g',
+      ingredientAmount: '2',
     },
     {
-      ingredientName: "sugar",
-      ingredientUnit: "count",
-      ingredientAmount: "1"
+      ingredientName: 'sugar',
+      ingredientUnit: 'count',
+      ingredientAmount: '1',
     }],
-    directions: ["first step"]
-  }
+    directions: ['first step'],
+  };
   expect(validateForm(recipe.name, recipe.directions, recipe.ingredients)).toEqual(true);
-})
+});
 
-it("the form should be invalid when ingredient amount is equal 0", () => {
+it('the form should be invalid when ingredient amount is equal 0', () => {
   const recipe = {
-    name: "a",
+    name: 'a',
     ingredients: [{
-      ingredientName: "sugar",
-      ingredientUnit: "g",
-      ingredientAmount: "0"
+      ingredientName: 'sugar',
+      ingredientUnit: 'g',
+      ingredientAmount: '0',
     }],
-    directions: ["first step"]
-  }
+    directions: ['first step'],
+  };
   expect(validateForm(recipe.name, recipe.directions, recipe.ingredients)).toEqual(true);
-})
+});
 
-it("the form should be invalid when ingredient amount is smaller than 0", () => {
+it('the form should be invalid when ingredient amount is smaller than 0', () => {
   const recipe = {
-    name: "a",
+    name: 'a',
     ingredients: [{
-      ingredientName: "sugar",
-      ingredientUnit: "g",
-      ingredientAmount: "-1"
+      ingredientName: 'sugar',
+      ingredientUnit: 'g',
+      ingredientAmount: '-1',
     }],
-    directions: ["first step"]
-  }
+    directions: ['first step'],
+  };
   expect(validateForm(recipe.name, recipe.directions, recipe.ingredients)).toEqual(true);
-})
+});
 
-it("the form should be valid when fields are correct", () => {
+it('the form should be valid when fields are correct', () => {
   const recipe = {
-    name: "a",
+    name: 'a',
     ingredients: [{
-      ingredientName: "sugar",
-      ingredientUnit: "g",
-      ingredientAmount: "2"
+      ingredientName: 'sugar',
+      ingredientUnit: 'g',
+      ingredientAmount: '2',
     }],
-    directions: ["first step"]
-  }
+    directions: ['first step'],
+  };
   expect(validateForm(recipe.name, recipe.directions, recipe.ingredients)).toEqual(false);
-})
+});
